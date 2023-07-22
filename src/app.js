e56e5824dd2170dda56efa4f7b417fd2dddce4c1
Antonio Robles <antjrobles@gmail.com>
2023-07-22 12:40:42 +0200

    Helmet added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
 app.listen(3022);
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
