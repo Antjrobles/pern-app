8d54e972bffbd396f59f1aa861ca7fbf9255f597
Antonio Robles <antjrobles@gmail.com>
2023-07-23 12:40:12 +0200

    routes

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,16 +1,15 @@
-import {Router} from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/tasks', (req, res) => res.send('obteniendo tares'));  
+router.get("/tasks", (req, res) => res.send("obteniendo tares"));
 
-router.get('/tasks/:id', (req, res) => res.send('obteniendo tarea unica'));  
+router.get("/tasks/:id", (req, res) => res.send("obteniendo tarea unica"));
 
-router.post('/tasks', (req, res) => res.send('creando tarea'));  
+router.post("/tasks", (req, res) => res.send("creando tarea"));
 
-router.put('/tasks/:id', (req, res) => res.send('acualizando tarea única'));  
+router.put("/tasks/:id", (req, res) => res.send("acualizando tarea única"));
 
-router.delete('/tasks/:id', (req, res) => res.send('eliminando tarea'));  
+router.delete("/tasks/:id", (req, res) => res.send("eliminando tarea"));
 
 export default router;
-
