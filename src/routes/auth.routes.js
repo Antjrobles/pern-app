16550be261cb1386ef051c948c4a06691080c1c9
Antonio Robles <antjrobles@gmail.com>
2023-07-23 18:49:20 +0200

    controllers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,19 +1,20 @@
 import { Router } from 'express';
+import { signin, signup, signout, profile } from '../controllers/auth.controller.js';
 
 
 const router = Router();
 
 
 
-router.post('/signin', (req, res) => res.send("ingresando"));
+router.post('/signin', signin);
 
 
-router.post('/signup', (req, res) => res.send('registrando'));  
+router.post('/signup', signup);  
 
 
-router.post('/signout', (req, res) => res.send('cerrando sesion'));
+router.post('/signout',signout );
 
 
-router.get('/profile', (req, res) => res.send('perfile del ususario'));  
+router.get('/profile', profile);  
 
 export default router;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
+import { getAllTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/tasks.controllers.js';
 
 const router = Router();
 
-router.get("/tasks", (req, res) => res.send("obteniendo tares"));
+router.get("/tasks", getAllTasks);
 
-router.get("/tasks/:id", (req, res) => res.send("obteniendo tarea unica"));
+router.get("/tasks/:id", getTask);
 
-router.post("/tasks", (req, res) => res.send("creando tarea"));
+router.post("/tasks", createTask);
 
-router.put("/tasks/:id", (req, res) => res.send("acualizando tarea única"));
+router.put("/tasks/:id", updateTask);
 
-router.delete("/tasks/:id", (req, res) => res.send("eliminando tarea"));
+router.delete("/tasks/:id", deleteTask);
 
 export default router;
